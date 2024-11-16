48ed1b81ffe20db05f2e0666a9390d93e142f052
Jreagn <113216795+Jreagn@users.noreply.github.com>
2024-11-16 12:43:35 -0500

    Update hehe.ps1
    Added comp-specific IP ranges

--- windows/hehe.ps1
+++ windows/hehe.ps1
@@ -1,6 +1,6 @@
 # Script used to configure restricted firewall rules for a Windows Box | Credit to Jregan for sharing
 net stop bthserv
-net stop dns
+# net stop dns
 net stop nettcpportsharing
 net stop rasman
 net stop remoteregistry
@@ -21,25 +21,29 @@ netsh advfirewall export "C:\Program Files\Common Files\default.wfw"
 Remove-NetFirewallRule -All
 
 # Baseline stuff, use on all machines
-# New-NetFirewallRule -DisplayName "Internal traffic" -Direction Inbound -RemoteAddress "172.20.0.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic" -Direction Inbound -RemoteAddress "172.20.240.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic" -Direction Inbound -RemoteAddress "172.20.241.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic" -Direction Inbound -RemoteAddress "172.20.242.0" -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "HTTP(S) external outbound" -Direction Outbound -RemotePort 80,443 -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "DNS Client" -Program "C:\Windows\system32\svchost.exe" -Direction Outbound -RemotePort 53 -Protocol UDP -Action Allow
-# New-NetFirewallRule -DisplayName "Internal traffic out" -Direction Outbound -RemoteAddress "172.20.0.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic out" -Direction Outbound -RemoteAddress "172.20.240.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic out" -Direction Outbound -RemoteAddress "172.20.241.0" -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Internal traffic out" -Direction Outbound -RemoteAddress "172.20.242.0" -Protocol TCP -Action Allow
 
 # Critical services, only uncomment the ones you need
 # New-NetFirewallRule -DisplayName "HTTP server" -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "HTTPS server" -Direction Inbound -LocalPort 443 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "FTP server IN" -Program "C:\Windows\system32\svchost.exe" -Direction Inbound -LocalPort 20,21,1024-65535 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "FTP server OUT" -Program "C:\Windows\system32\svchost.exe" -Direction Outbound -LocalPort 20 -Protocol TCP -Action Allow
-# New-NetFirewallRule -DisplayName "DNS server" -Program "C:\Windows\system32\dns.exe" -Direction Inbound -LocalPort 53 -Protocol TCP -Action Allow
-# New-NetFirewallRule -DisplayName "DNS server" -Program "C:\Windows\system32\dns.exe" -Direction Inbound -LocalPort 53 -Protocol UDP -Action Allow
-# New-NetFirewallRule -DisplayName "RDP server" -Program "C:\Windows\system32\svchost.exe" -Direction Inbound -LocalPort 3389 -Protocol TCP -Action Allow
-# New-NetFirewallRule -DisplayName "RDP server" -Program "C:\Windows\system32\svchost.exe" -Direction Inbound -LocalPort 3389 -Protocol UDP -Action Allow
+New-NetFirewallRule -DisplayName "DNS server" -Program "C:\Windows\system32\dns.exe" -Direction Inbound -LocalPort 53 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "DNS server" -Program "C:\Windows\system32\dns.exe" -Direction Inbound -LocalPort 53 -Protocol UDP -Action Allow
+New-NetFirewallRule -DisplayName "RDP server" -Program "C:\Windows\system32\svchost.exe" -Direction Inbound -LocalPort 3389 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "RDP server" -Program "C:\Windows\system32\svchost.exe" -Direction Inbound -LocalPort 3389 -Protocol UDP -Action Allow
 # New-NetFirewallRule -DisplayName "SMB server" -Direction Inbound -LocalPort 445 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "SSH server" -Direction Inbound -LocalPort 22 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "SMTP server" -Direction Inbound -LocalPort 23,110,143,465,587,993,995,2525 -Protocol TCP -Action Allow
-# New-NetFirewallRule -DisplayName "DHCP server" -Direction Inbound -LocalPort 67,68 -Protocol TCP -Action Allow
-# New-NetFirewallRule -DisplayName "DHCP server" -Direction Inbound -LocalPort 67,68 -Protocol UDP -Action Allow
+New-NetFirewallRule -DisplayName "DHCP server" -Direction Inbound -LocalPort 67,68 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "DHCP server" -Direction Inbound -LocalPort 67,68 -Protocol UDP -Action Allow
 # New-NetFirewallRule -DisplayName "POP3 scoring" -Direction Inbound -LocalPort 110 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "POP3 scoring" -Direction Outbound -LocalPort 110 -Protocol TCP -Action Allow
 # New-NetFirewallRule -DisplayName "POP3 scoring UDP" -Direction Inbound -LocalPort 110 -Protocol UDP -Action Allow
