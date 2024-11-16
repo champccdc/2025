0bd677c2d1e0b66ff32536b80331539a8f5b2a6d
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-11-16 12:42:35 -0500

    Create Install_Tools.ps1

---
+++ windows/Install_Tools.ps1
@@ -0,0 +1,14 @@
+# Script used to Download sysinternals, Firewall rules, and nmap
+
+# How To Use - Run the commands below on a Win System:
+# Invoke-WebRequest -Uri '<content-url-goes-here' -OutFile .\Install_Tools.ps1
+# .\Install_Tools.ps1
+
+# Downloading Sysinternals Suite
+Invoke-WebRequest 'https://download.sysinternals.com/files/SysinternalsSuite.zip' -OutFile .\
+
+# Downloading Firewall Script
+Invoke-WebRequest 'https://raw.githubusercontent.com/champccdc/2025/refs/heads/main/windows/hehe.ps1' -OutFile .\hehe.ps1
+
+# Downloading + Installing Nmap
+Invoke-WebRequest 'https://nmap.org/dist/nmap-7.95-setup.exe' -OutFile .\'nmap-7.95-setup.exe'
