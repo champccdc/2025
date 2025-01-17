561313b2d192401ae66d1480e959d2e7fb7410d9
Liam Gugliotta <liam.gugliotta@mymail.champlain.edu>
2025-01-17 12:02:23 -0500

    Updated linux/listEditedFiles.bash  fixed some problems and added more options

--- linux/listEditedFiles.bash
+++ linux/listEditedFiles.bash
@@ -1,20 +1,27 @@
 #!/bin/bash
-usage() { echo "Usage: $0 [-E -L -t <int> -s <d | m> -p <string>]"; exit 1; }
+usage() { echo "Usage: $0 [-E -L -t <int> -s <d | m> -p <string> -f <file> -o <file>]"; exit 1; }
+
+#i know this looks absolutely atrocious but too lazy to see how to compact it
 
 #E option excludes /etc/ directories
 #L option excludes /var/log/ directories
 #t <int> option sets how far back fxn looks for changes USE NEG INT
 #s <d | m> option changes time scale to days or minutes
 #p <string> option adds a file path to the list of directories to search
+#f option requires a file that contains a list of directories to search seperated by newline
+#o option takes a file to output results to instead of printing to console
+#FUTURE WORK: sort/filter entries by priority
 
 excludeETC=0
 excludeVARLOG=0
 filePath=""
+fileName=""
+outputFile=""
 duration=-1
 scale="d"
 possibleScales="dm"
 
-while getopts "ELt:s:p:" arg
+while getopts "ELt:s:p:f:o:" arg
 do
   case $arg in
     E)
@@ -25,7 +32,7 @@ do
       ;;
     t)
       duration=$OPTARG
-      [ $OPTARG > 0 ] && duration=$(($OPTARG-$OPTARG*2))
+      [ $OPTARG -gt 0 ] && duration=$(($OPTARG-$OPTARG*2))
       ;;
     p)
       filePath=$OPTARG
@@ -34,24 +41,60 @@ do
       if [[ "$possibleScales" =~ "$OPTARG" ]]; then
         scale=$OPTARG
       else
-        echo "OPTARG not in possible scales"
-        
+        echo "OPTARG not in possible scales" 
         usage
       fi
+      ;;
+    f)
+      fileName=$OPTARG
+      ;;
+    o)
+      outputFile=$OPTARG
       ;;
     *)
       usage
       ;;
   esac
 done
-
-if [[ $scale == "d" ]]; then
-  [ $excludeETC -eq 0 ] && find /etc/* -mtime $duration -print
-  [ $excludeVARLOG -eq 0 ] && find /var/log/* -mtime $duration -print
-  [[ -n "$filePath" ]] && find $filePath -mtime $duration -print
+if [[ -n "$outputFile" ]]; then
+  :> $outputFile
+  if [[ $scale == "d" ]]; then
+    [[ $excludeETC == 0 ]] && find /etc/ -mtime $duration -print >> $outputFile
+    [[ $excludeVARLOG == 0 ]] && find /var/log/ -mtime $duration -print >> $outputFile
+    [[ -n "$filePath" ]] && find $filePath -mtime $duration -print >> $outputFile
+    if [[ -n "$fileName" ]]; then
+      while read -r line; do
+        find $line -mtime $duration -print >> $outputFile
+      done < $fileName
+    fi
+  else
+    [[ $excludeETC -eq 0 ]] && find /etc/* -mmin $duration -print >> $outputFile
+    [[ $excludeVARLOG  -eq 0 ]] && find /var/log/* -mmin $duration -print >> $outputFile
+    [[ -n "$filePath" ]] && find $filePath -mmin $duration -print >> $outputFile
+    if [[ -n "$fileName" ]]; then
+      while read -r line; do
+        find $line -mmin $duration -print >> $outputFile
+      done < $fileName
+    fi
+  fi
 else
-  [ $excludeETC -eq 0 ] && find /etc/* -mmin $duration -print
-  [ $excludeVARLOG -eq 0 ] && find /var/log/* -mmin $duration -print
-  [[ -n "$filePath" ]] && find $filePath -mtime $duration -print
+  if [[ $scale == "d" ]]; then
+    [ $excludeETC -eq 0 ] && find /etc/* -mtime $duration -print
+    [ $excludeVARLOG -eq 0 ] && find /var/log/* -mtime $duration -print
+    [[ -n "$filePath" ]] && find $filePath -mtime $duration -print
+    if [[ -n "$fileName" ]]; then
+      while read -r line; do
+        find $line -mtime $duration -print
+      done < $fileName
+    fi
+  else
+    [ $excludeETC -eq 0 ] && find /etc/* -mmin $duration -print
+    [ $excludeVARLOG -eq 0 ] && find /var/log/* -mmin $duration -print
+    [[ -n "$filePath" ]] && find $filePath -mmin $duration -print
+    if [[ -n "$fileName" ]]; then
+      while read -r line; do
+        find $line -mmin $duration -print
+      done < $fileName
+    fi
+  fi
 fi
-
