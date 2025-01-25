6b7bf90cc0685b0f43471ddb9dcf76fd44a0a166
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-01-24 23:58:37 -0500

    Update Install_Tools.ps1

--- windows/Install_Tools.ps1
+++ windows/Install_Tools.ps1
@@ -14,7 +14,6 @@ Invoke-WebRequest 'https://nmap.org/dist/nmap-7.95-setup.exe' -OutFile .\'nmap-7
 Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US&attribution_code=c291cmNlPXd3dy5nb29nbGUuY29tJm1lZGl1bT1yZWZlcnJhbCZjYW1wYWlnbj0obm90IHNldCkmY29udGVudD0obm90IHNldCkmZXhwZXJpbWVudD0obm90IHNldCkmdmFyaWF0aW9uPShub3Qgc2V0KSZ1YT1jaHJvbWUmY2xpZW50X2lkX2dhND0obm90IHNldCkmc2Vzc2lvbl9pZD0obm90IHNldCkmZGxzb3VyY2U9bW96b3Jn&attribution_sig=c629f49b91d2fa3e54e7b9ae8d92a74866b3980356bf1a5b70c0bca69812620b' -OutFile .\'Firefox Install.exe'
 
 # Acct Stuff
-
 Invoke-WebRequest 'https://raw.githubusercontent.com/champccdc/2025/refs/heads/main/windows/Acct_Check.ps1' -OutFile .\Acct_Check.ps1
 
 # Downloading Sysinternals Suite
